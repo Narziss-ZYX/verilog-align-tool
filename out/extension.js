dc68fa7eef3cee6eb2202bbf20d17034f388a052
Narziss-ZYX <1977148195@qq.com>
2022-06-09 14:51:28 +0800

    增加选中括号再对齐命令

--- out/extension.js
+++ out/extension.js
@@ -5,43 +5,40 @@ exports.deactivate = exports.activate = void 0;
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const block_1 = require("./block");
+async function overwriteText(textEditor) {
+    let selection = textEditor.selection;
+    if (!selection.isEmpty) {
+        let textDocument = textEditor.document;
+        // Don't select last line, if no character of line is selected.
+        let endLine = selection.end.line;
+        let endPosition = selection.end;
+        if (endPosition.character === 0) {
+            endLine--;
+        }
+        let range = new vscode.Range(new vscode.Position(selection.start.line, 0), new vscode.Position(endLine, textDocument.lineAt(endLine).range.end.character));
+        let text = textDocument.getText(range);
+        let block = new block_1.Block(text, selection.start.line, textDocument.eol).trim().align();
+        await textEditor.edit(e => {
+            for (let line of block.lines) {
+                let deleteRange = new vscode.Range(new vscode.Position(line.number, 0), new vscode.Position(line.number, textDocument.lineAt(line.number).range.end.character));
+                let replacement = '';
+                for (let part of line.parts) {
+                    replacement += part.value;
+                }
+                e.replace(deleteRange, replacement);
+            }
+        });
+    }
+}
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "verilog-align-tool" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let verilogAlignTool = vscode.commands.registerTextEditorCommand('verilog.align.tool', async (textEditor, edit, args) => {
-        // The code you place here will be executed every time your command is executed
-        // vscode.window.showInformationMessage('Hello World from verilog-align-tool!');
-        let selection = textEditor.selection;
-        if (!selection.isEmpty) {
-            let textDocument = textEditor.document;
-            // Don't select last line, if no character of line is selected.
-            let endLine = selection.end.line;
-            let endPosition = selection.end;
-            if (endPosition.character === 0) {
-                endLine--;
-            }
-            let range = new vscode.Range(new vscode.Position(selection.start.line, 0), new vscode.Position(endLine, textDocument.lineAt(endLine).range.end.character));
-            let text = textDocument.getText(range);
-            let block = new block_1.Block(text, selection.start.line, textDocument.eol).trim().align();
-            await textEditor.edit(e => {
-                for (let line of block.lines) {
-                    let deleteRange = new vscode.Range(new vscode.Position(line.number, 0), new vscode.Position(line.number, textDocument.lineAt(line.number).range.end.character));
-                    let replacement = '';
-                    for (let part of line.parts) {
-                        replacement += part.value;
-                    }
-                    e.replace(deleteRange, replacement);
-                }
-            });
-        }
-    });
-    context.subscriptions.push(verilogAlignTool);
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand('verilog.align.tool', async (textEditor, edit, args) => {
+        overwriteText(textEditor);
+    }), vscode.commands.registerTextEditorCommand('verilog.align.tool.withBracket', async (textEditor, edit, args) => {
+        await vscode.commands.executeCommand('editor.action.selectToBracket');
+        overwriteText(textEditor);
+    }));
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -3,47 +3,46 @@
 import * as vscode from 'vscode';
 import { Block } from './block';
 
+async function overwriteText(textEditor: vscode.TextEditor){
+	let selection: vscode.Selection = textEditor.selection;
+	if (!selection.isEmpty) {
+		let textDocument = textEditor.document;
+		
+		// Don't select last line, if no character of line is selected.
+		let endLine = selection.end.line;
+		let endPosition = selection.end;
+		if(endPosition.character === 0) {
+			endLine--;
+		}
+
+		let range = new vscode.Range(new vscode.Position(selection.start.line, 0), new vscode.Position(endLine, textDocument.lineAt(endLine).range.end.character));
+		let text = textDocument.getText(range);
+		let block : Block = new Block(text, selection.start.line, textDocument.eol).trim().align();
+		await textEditor.edit(e => {
+			for (let line of block.lines) {
+				let deleteRange = new vscode.Range(new vscode.Position(line.number, 0), new vscode.Position(line.number, textDocument.lineAt(line.number).range.end.character));
+				let replacement: string = '';
+				for (let part of line.parts) {
+					replacement += part.value;
+				}
+				e.replace(deleteRange, replacement);
+			}
+		});
+	}
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "verilog-align-tool" is now active!');
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let verilogAlignTool = vscode.commands.registerTextEditorCommand('verilog.align.tool', async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, args: any[]) => {
-		// The code you place here will be executed every time your command is executed
-		// vscode.window.showInformationMessage('Hello World from verilog-align-tool!');
-		let selection: vscode.Selection = textEditor.selection;
-		if (!selection.isEmpty) {
-			let textDocument = textEditor.document;
-			
-			// Don't select last line, if no character of line is selected.
-			let endLine = selection.end.line;
-			let endPosition = selection.end;
-			if(endPosition.character === 0) {
-				endLine--;
-			}
-
-			let range = new vscode.Range(new vscode.Position(selection.start.line, 0), new vscode.Position(endLine, textDocument.lineAt(endLine).range.end.character));
-            let text = textDocument.getText(range);
-			let block : Block = new Block(text, selection.start.line, textDocument.eol).trim().align();
-			await textEditor.edit(e => {
-				for (let line of block.lines) {
-					let deleteRange = new vscode.Range(new vscode.Position(line.number, 0), new vscode.Position(line.number, textDocument.lineAt(line.number).range.end.character));
-					let replacement: string = '';
-					for (let part of line.parts) {
-						replacement += part.value;
-					}
-					e.replace(deleteRange, replacement);
-				}
-			});
-		}
-		
-	});
-	context.subscriptions.push(verilogAlignTool);
+	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand('verilog.align.tool', async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, args: any[]) => {
+			overwriteText(textEditor);
+		}),
+		vscode.commands.registerTextEditorCommand('verilog.align.tool.withBracket', async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, args: any[]) => {
+			await vscode.commands.executeCommand('editor.action.selectToBracket');
+			overwriteText(textEditor);
+		})
+	);
 }
 
 // this method is called when your extension is deactivated
